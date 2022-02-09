434b2ee0969dbb7ce2d0e38814ac9a4d24f1ae10
Bodhi Hawken <36744662+Hawki101@users.noreply.github.com>
2022-02-09 16:24:10 +1100

    Delete utils.spec.js

--- __tests__/utils.spec.js
+++
@@ -1,6 +0,0 @@
-import { formatChannelName, buildSlackAttachments } from '../src/utils';
-import { GITHUB_PUSH_EVENT, GITHUB_PR_EVENT } from '../fixtures';
-const runId = parseInt(process.env.GITHUB_RUN_ID, 10);
-
-describe('Utils', () => {
-});
